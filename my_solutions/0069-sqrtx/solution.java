56c3d1b17ec3e2189e67b93d89957ec2f60e0843
Aniket Singhal <142080618+AniketS1712@users.noreply.github.com>
2026-01-13 06:08:34 +0000

    Sync LeetCode submission Runtime - 28 ms (5.15%), Memory - 43 MB (7.22%)

--- my_solutions/0069-sqrtx/solution.java
+++ my_solutions/0069-sqrtx/solution.java
@@ -1,19 +1,33 @@
 class Solution {
     public int mySqrt(int x) {
-        int low = 0;
-        int mid;
-        int high = x;
-        int sqr = 0;
+        // int low = 0;
+        // int mid;
+        // int high = x;
+        // int sqr = 0;
         
-        while(low <= high) {
-            mid = (low + high) / 2;
-            long square = (long) mid * mid;
-            if(square == x) return mid;
-            else if(square <= x) {
-                low = mid + 1;
-                sqr = mid;
-            } else {
-                high = mid - 1;;
+        // while(low <= high) {
+        //     mid = (low + high) / 2;
+        //     long square = (long) mid * mid;
+        //     if(square == x) return mid;
+        //     else if(square <= x) {
+        //         low = mid + 1;
+        //         sqr = mid;
+        //     } else {
+        //         high = mid - 1;;
+        //     }
+        // }
+        // return sqr;
+
+        // Subtraction Method
+        int i = 1;
+        int sqr = 0;
+        while(x != 0 || x < 0) {
+            if(x > 0) {
+                x -= i;
+                i += 2;
+                sqr++;
+            } else if(x < 0){
+                return sqr - 1;
             }
         }
         return sqr;
